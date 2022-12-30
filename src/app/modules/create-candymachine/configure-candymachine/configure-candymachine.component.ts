0745a6af8264a0abc00cd3b6202f032d2dc9d72d
nathanielremy <nathanielremy1@gmail.com>
2022-12-30 18:10:11 -0400

    Solana and Metaplex connections validated pre CM creation

--- src/app/modules/create-candymachine/configure-candymachine/configure-candymachine.component.ts
+++ src/app/modules/create-candymachine/configure-candymachine/configure-candymachine.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Adapter } from '@solana/wallet-adapter-base';
+import { MetaplexService } from 'src/app/service/metaplex.service';
+import { SnackService } from 'src/app/service/snack.service';
+import { WalletService } from 'src/app/service/wallet.service';
 
 @Component({
   selector: 'app-configure-candymachine',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configure-candymachine.component.css']
 })
 export class ConfigureCandymachineComponent implements OnInit {
+  walletAdapter: Adapter | null = null;
+  rpcEndpoint: string | null = null;
 
-  constructor() { }
+  constructor(
+    private walletService: WalletService,
+    private mxService: MetaplexService,
+    private snackService: SnackService
+  ) { }
 
   ngOnInit(): void {
+    this.walletService.walletStore$.wallet$.subscribe(
+      (wallet) => { this.walletAdapter = wallet?.adapter }
+    );
+    this.walletService.connectionStore$.state$.subscribe(
+      (state) => { this.rpcEndpoint = state.endpoint }
+    );
   }
 
+  createCandyMachine() {
+    if (this.validateUserConnection()) {
+      this.snackService.showSuccess('Creating candymachine');
+    }
+  }
+
+  validateUserConnection(): boolean {
+    // Validate wallet is connected
+    if (this.walletAdapter?.connected && this.walletAdapter?.publicKey) {
+      if (this.rpcEndpoint) {
+        // Validate rpcEndpoint and wallet matches metaplex instance
+        if (this.mxService.validateInstance(this.rpcEndpoint, this.walletAdapter)) {
+          return true;
+        } else {
+          this.snackService.showError(
+            'Error connecting to network, please disconnect, reload and try again'
+          );
+          return false;
+        }
+      } else {
+        this.snackService.showError('Rpc error, please reload and try again');
+        return false;
+      }
+    } else {
+      this.snackService.showError('Wallet not connected');
+      return false;
+    }
+  }
 }
