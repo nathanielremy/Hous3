07f7c32a4d620bc668d93dae3d3985cbccac7e08
nathanielremy <nathanielremy1@gmail.com>
2023-01-03 13:37:51 -0400

    Create candymachine with user input values

--- src/app/modules/create-candymachine/configure-candymachine/configure-candymachine.component.ts
+++ src/app/modules/create-candymachine/configure-candymachine/configure-candymachine.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { MetaplexError, toBigNumber } from '@metaplex-foundation/js';
+import { CreateCandyMachineInput, MetaplexError, toBigNumber } from '@metaplex-foundation/js';
 import { PublicKey } from '@solana/web3.js';
 import { Adapter } from '@solana/wallet-adapter-base';
 import { MetaplexService } from 'src/app/service/metaplex.service';
 import { SnackService } from 'src/app/service/snack.service';
 import { WalletService } from 'src/app/service/wallet.service';
+import { isValidSolanaAddress, truncateAddress } from 'src/app/common/utils/utils';
 
 @Component({
   selector: 'app-configure-candymachine',
@@ -15,11 +16,27 @@ export class ConfigureCandymachineComponent implements OnInit {
   walletAdapter: Adapter | null = null;
   rpcEndpoint: string | null = null;
 
+  royalty: number;
+  supply: number;
+  symbol: string = '';
+  isMutable: boolean = true;
+  creators: {
+    address: string,
+    verified: boolean,
+    share: number
+  }[] = [];
+
   constructor(
     private walletService: WalletService,
     private mxService: MetaplexService,
     private snackService: SnackService
   ) { }
+
+  get updateAuthorityPlaceholder(): string {
+    return this.walletAdapter?.publicKey?.toString()
+      ? truncateAddress(this.walletAdapter?.publicKey?.toString())
+      : 'Connect wallet';
+  }
 
   ngOnInit(): void {
     this.walletService.walletStore$.wallet$.subscribe(
@@ -30,30 +47,36 @@ export class ConfigureCandymachineComponent implements OnInit {
     );
   }
 
+  addCreator() {
+    for (let creator of this.creators) {
+      if (!isValidSolanaAddress(creator.address)) {
+        this.snackService.showError(
+          'Please enter a valid Solana wallet address'
+        );
+        return;
+      }
+    }
+    if (this.creators.length < 4) {
+      this.creators.push({
+        address: '',
+        verified: true,
+        share: 0
+      })
+    }
+  }
+
+  deleteCreator(idx: number) {
+    this.creators.splice(idx, 1);
+  }
+
   async createCandyMachine() {
     if (this.validateUserConnection()) {
       try {
-        const candyMachineSettings = {
-          authority: this.mxService.getIdentity(),
-          collection: {
-            address: new PublicKey('7ymeaNmfLZ6SCyVRvupL9tpZaLa5fMJpwHTNYhjob4uQ'),
-            updateAuthority: this.mxService.getIdentity()
-          },
-          sellerFeeBasisPoints: 5,
-          itemsAvailable: toBigNumber(5),
-          itemSettings: undefined, // CandyMachineHiddenSettings | CandyMachineConfigLineSettings
-          symbol: 'CM_SYMBOL',
-          maxEditionSupply: toBigNumber(0),
-          isMutable: true,
-          creators: [{
-            address: this.mxService.getIdentity().publicKey,
-            verified: true,
-            share: 100
-          }]
-        };
-
-        const candyMachine = await this.mxService.createCandyMachine(candyMachineSettings);
-        console.log(`CANDYMACHINE: ${JSON.stringify(candyMachine.candyMachine)}`);
+        const candyMachineSettings = this.constructCandyMachineSettings();
+        if (candyMachineSettings) {
+          const candyMachine = await this.mxService.createCandyMachine(candyMachineSettings);
+          console.log(`CANDYMACHINE: ${JSON.stringify(candyMachine.candyMachine)}`);
+        }
       }
       catch (err) {
         this.snackService.showError(
@@ -62,6 +85,82 @@ export class ConfigureCandymachineComponent implements OnInit {
         console.log(`err: ${err}`);
       }
     }
+  }
+
+  constructCandyMachineSettings(): CreateCandyMachineInput | null {
+    if (!this.royalty || this.royalty < 0 || this.royalty > 10) {
+      this.snackService.showWarning('Please enter a royalty percentage between 0 - 10%');
+      return null;
+    }
+
+    if (!this.supply || this.supply < 0 || this.supply > 10000) {
+      this.snackService.showWarning('Please enter a supply between 0 - 10k');
+      return null;
+    }
+
+    if (this.supply != Math.round(this.supply)) {
+      this.snackService.showWarning('Supply must be a round number');
+      return null;
+    }
+
+    if (!this.symbol.trim() || this.symbol.length > 10) {
+      this.snackService.showWarning('Please enter a valid symbol. Max 10 characters');
+      return null;
+    }
+
+    if (this.creators.length < 5) {
+      var sumOfCreatorShares = 0;
+      for (let creator of this.creators) {
+        if (!isValidSolanaAddress(creator.address)) {
+          this.snackService.showWarning(
+            'Please enter valid Solana wallet addresses for creators'
+          );
+          return null;
+        }
+        sumOfCreatorShares += creator.share;
+      }
+      if (
+        this.creators.length > 0 &&
+        (sumOfCreatorShares < 100 || sumOfCreatorShares > 100)
+      ) {
+        this.snackService.showWarning('Creator shares must sum up to 100%');
+        return null;
+      }
+    } else {
+      this.snackService.showWarning('Please enter 4 or less creators');
+      return null;
+    }
+
+    console.log(`Royalty: ${this.royalty}`)
+    console.log(`Supply: ${this.supply}`)
+    console.log(`Symbol: ${this.symbol}`)
+    console.log(`isMutable: ${this.isMutable}`)
+    for (let creator of this.creators) {
+      console.log(`Creator: ${truncateAddress(creator.address)} ${creator.share}`)
+    }
+
+    const candyMachineSettings = {
+      authority: this.mxService.getIdentity(),
+      collection: {
+        address: new PublicKey('7ymeaNmfLZ6SCyVRvupL9tpZaLa5fMJpwHTNYhjob4uQ'),
+        updateAuthority: this.mxService.getIdentity()
+      },
+      sellerFeeBasisPoints: Math.trunc(this.royalty * 100),
+      itemsAvailable: toBigNumber(this.supply),
+      itemSettings: undefined, // CandyMachineHiddenSettings | CandyMachineConfigLineSettings
+      symbol: this.symbol,
+      maxEditionSupply: toBigNumber(0),
+      isMutable: this.isMutable,
+      creators: this.creators.map((creator) => {
+        return {
+          address: new PublicKey(creator.address),
+          verified: creator.verified,
+          share: creator.share
+        }
+      })
+    };
+
+    return candyMachineSettings;
   }
 
   validateUserConnection(): boolean {
